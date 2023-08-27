ee55728535148188c97d077d9405261df3a9b2b5
gronck2 <astepanov.waves@gmail.com>
2023-08-27 16:22:35 +0400

    delete function `get_dp`

--- distance.py
+++ distance.py
@@ -14,24 +14,20 @@ def minimal_distance(word1: str, word2: str):
     m = len(word2)
     dp = create_matrix(n, m)
 
-    def get_dp(i, j):
-        if i < 0 or j < 0:
-            return 0
-        if i < 0:
-            return j + 1
-        if j < 0:
-            return i + 1
-        return dp[i][j]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 0:
+                dp[i][j] = j  # Initialize the first row
+            elif j == 0:
+                dp[i][j] = i  # Initialize the first column
+            else:
+                dp[i][j] = min(
+                    dp[i-1][j] + 1,
+                    dp[i][j-1] + 1,
+                    dp[i-1][j-1] + (0 if word1[i-1] == word2[j-1] else 1)
+                )
 
-    for i in range(n):
-        for j in range(m):
-            dp[i][j] = min(
-                get_dp(i-1, j) + 1,
-                get_dp(i, j-1) + 1,
-                get_dp(i-1, j-1) + (0 if word1[i] == word2[j] else 1)
-            )
-
-    distance = get_dp(n-1, m-1)
+    distance = dp[n-1][m-1]
     print(distance)
     cur_i = n-1
     cur_j = m-1
@@ -39,9 +35,9 @@ def minimal_distance(word1: str, word2: str):
 
     print(''.join(cur_word))
     while distance > 0:
-        deletion = get_dp(cur_i, cur_j-1)
-        insertion = get_dp(cur_i-1, cur_j)
-        substitution = get_dp(cur_i-1, cur_j-1)
+        deletion = dp[cur_i][cur_j-1]
+        insertion = dp[cur_i-1][cur_j]
+        substitution = dp[cur_i-1][cur_j-1]
         if substitution < distance:
             cur_word[cur_j] = word1[cur_i]
             cur_i -= 1
@@ -61,6 +57,7 @@ def minimal_distance(word1: str, word2: str):
         else:
             cur_i -= 1
             cur_j -= 1
+
 
 if __name__ == '__main__':
     import sys
